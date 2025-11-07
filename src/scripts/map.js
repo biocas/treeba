4b1ed02df1bb5c937cf46f4eb69a219f8f790aa4
Margarida <biasalvado@gmail.com>
2025-11-07 17:12:31 +0000

    map basic setup

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
 mapboxgl.accessToken = 'pk.eyJ1IjoibWFyZ2FyaWRhc2FsdmFkbyIsImEiOiJja2pndWUzMm80ZmowMnFwZDVxYmt5NWZjIn0.wh2-Kf9dve6BZJGX2hEjEw';
  const map = new mapboxgl.Map({
     container: 'treeba-map',
-    style: 'mapbox://styles/margaridasalvado/cmhnwbnfb00cy01sh25fp9wvw', //hosted style id
+    style: 'mapbox://styles/margaridasalvado/cmhnxyl7t000n01s50tgnhgtg', //hosted style id
     center: [-2.652385, 40.075565], // starting position
     zoom: 3.8
 });
