d6ab7a5b76fd846baa8be0b4d57decfd0d6c60f3
Margarida <biasalvado@gmail.com>
2025-11-10 18:29:57 +0000

    all layers in and setup funcionality on the map

--- src/scripts/solarmap.js
+++ src/scripts/solarmap.js
@@ -13,5 +13,48 @@ let popup = new mapboxgl.Popup({
         className: 'map-popup'
         });
     let hoveredFeatureId = null;
+// Add zoom and rotation controls to the map.
+    map.addControl(new mapboxgl.NavigationControl());
+
+    map.on('load', function() { 
+//get all the layer IDs 
+let mapLayers = map.getStyle().layers;
+// Filter out the layers where visibility is "none"
+const visibleLayers = mapLayers.filter(layer => {
+  return !(layer.layout && layer.layout.visibility === 'none' || layer.id === 'NUTS' || layer.id === 'NUTS Nomes');
+});
+console.log(visibleLayers);
+//popup functionality
+visibleLayers
+  .forEach(l => {
+    map.on('click', l.id, (e) => {
+      const f = e.features[0];
+      if (!f) return;
+    //  var zoneName = e.features[0].properties.name;
+      const props = f.properties;
+      const layerTitle = f.layer?.id; // you can replace this with a custom title map if needed
+
+const html = `
+  <div class="popupLayerTitle"><strong>${layerTitle}</strong></div>
+  ${Object.entries(props)
+    .map(([k, v]) => `
+      <div class="popupProperty">
+        <span class="${k} popupPropertyTitle">${k}:</span>
+        <span class="${v} popupPropertyValue">${v}</span>
+      </div>
+    `)
+    .join('')}
+`;
+
+    //  const html = Object.entries(props).map(([k, v]) => `<div><span class="${k} popupPropertyTitle">${k}: </span><span class="${v} popupPropertyValue" ${v}</div>`).join('');
+      new mapboxgl.Popup().setLngLat(e.lngLat).setHTML(html).addTo(map);
+    });
+
+    map.on('mouseenter', l.id, () => map.getCanvas().style.cursor = 'pointer');
+    map.on('mouseleave', l.id, () => map.getCanvas().style.cursor = '');
+  });
+
+
+     });
 
 });
